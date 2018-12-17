8edfe8128aa1578c40926fe5a556d9430549a042
ThoPoi <thomas.poirier@hetic.net>
2018-12-17 17:20:25 +0100

    pop-up error finish

--- js/main.js
+++ js/main.js
@@ -9,62 +9,80 @@ const $popUpname = $formStory.querySelector('.popup-name')
 const $storyArea = $formStory.querySelector('.story-area')
 const $buttonReturn = document.querySelector('.button-return')
 
+
 let userName, userStory, errorRequest
 
-const popUpInsert = (status) =>
+const popUpInsert = () =>
 {
+
     let popUp = document.createElement('div')
     popUp.classList.add('popup--display')
+    popUp.setAttribute('display', 'flex')
+    popUp.setAttribute('justify-content', 'center')
+    popUp.setAttribute('align-items', 'center')
+
+    let imagePopUp = document.createElement('img')
     let textPopUp = document.createElement('p')
-    let buttonReturn = document.createElement('button')
-    buttonReturn.innerHTML = 'Retour'
-    buttonReturn.classList.add('test')
+    let buttonReturn = document.createElement('div')
+    let arrowReturn = document.createElement('div')
+
+    buttonReturn.classList.add('round-return')
+    buttonReturn.classList.add('button-return')
+
+    arrowReturn.classList.add('arrow-return')
+
+
+    let status = 200
     switch (status) {
         case 200:
             textPopUp.innerHTML = 'Histoire envoyé'
+
             break;
         case 404:
             textPopUp.innerHTML = 'Le serveur à un problème, nos techniciens travaillent dessus'
+            imagePopUp.setAttribute('src', '../images/pop-up/settings.svg')
+
             break;
         case 405:
             textPopUp.innerHTML = 'Vous avez un problème de connexion'
+            imagePopUp.setAttribute('src', '../images/pop-up/wifi.svg')
+
             break;
         case 400:
             textPopUp.innerHTML = 'Le texte est trop court'
+            imagePopUp.setAttribute('src', '../images/pop-up/cross.svg')
+
             break;
         default:
             textPopUp.innerHTML = 'Erreur, nous travaillons activement dessus'
 
     }
+
+    imagePopUp.setAttribute('width', '10' + '%')
     $popUpname.classList.remove('popup--display')
+
+
+    popUp.appendChild(imagePopUp)
     popUp.appendChild(textPopUp)
     popUp.appendChild(buttonReturn)
+    buttonReturn.appendChild(arrowReturn)
+    
     $formStory.insertBefore(popUp, $popUpname);
 
-    let buttonReturn1 = document.querySelector('.test')
+    let buttonReturn1 = document.querySelector('.button-return')
 
     buttonReturn1.addEventListener('click', (_event) => 
     {
         _event.preventDefault()
         $popUpname.classList.remove('popup--display')
         $storyArea.classList.remove('story-area--none')
+        $buttonNext.classList.remove('button-next--none')
         $storyArea.removeAttribute('disabled', 'none')
         popUp.remove()
         
     })
 
 }
-
-$buttonReturn.addEventListener('click', (_event) => 
-{
-        _event.preventDefault()
-        $popUpname.classList.remove('popup--display')
-        $storyArea.classList.remove('story-area--none')
-        $storyArea.removeAttribute('disabled', 'none')
-    
-})
-
-
 
 $submit.addEventListener('click', _event => 
 {
@@ -77,19 +95,28 @@ $submit.addEventListener('click', _event =>
             user_name: userName,
             user_story: userStory
     })
+
 })
 
 
+$buttonReturn.addEventListener('click', (_event) => 
+{
+        _event.preventDefault()
+        $popUpname.classList.remove('popup--display')
+        $storyArea.classList.remove('story-area--none')
+        $buttonNext.classList.remove('button-next--none')
 
-
+        $storyArea.removeAttribute('disabled', 'none')
+    
+})
 
 $buttonNext.addEventListener('click', (_event) => 
 {
     _event.preventDefault()
     $popUpname.classList.add('popup--display')
     $storyArea.classList.add('story-area--none')
+    $buttonNext.classList.add('button-next--none')
     $storyArea.setAttribute('disabled', 'none')
-
 })
 
 
