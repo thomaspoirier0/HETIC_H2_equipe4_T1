e388fc746601179e529a325ffd6f07ce14839dd0
ThoPoi <thomas.poirier@hetic.net>
2018-12-18 19:04:22 +0100

    moodInterpreter add

---
+++ js/moodInterpreter.js
@@ -0,0 +1,46 @@
+// example of using the interpreter
+fetch('./testvide.json')
+.then(res => res.json())
+.then((data) => {
+    
+    // console.log(data)
+
+    const moodInterpreter = new MoodInterpreter(data.mood)
+
+    const test = moodParser.getDominantMood()
+
+    console.log(test)
+});
+
+
+class MoodInterpreter {
+
+    // expects the 'mood' array to be passed to check the dominant mood of the text
+    constructor(moods) {
+        this.moods = moods
+        this.dominantScore = 0
+        this.dominantMood = null
+
+        this.getDominantMood = this.getDominantMood.bind(this)
+        this.getDominantMood()
+    }
+
+    getDominantMood() {
+        if (this.moods[0].score) {
+            console.log('case 1')
+            this.moods.forEach(mood => {
+                if (
+                    mood.score > this.dominantScore && 
+                    mood.tone_id !== 'analytical'
+                    ) {
+                    this.dominantScore = mood.score
+                    this.dominantMood = mood.tone_id
+                }
+            });
+            
+            return this.dominantMood
+        } else {
+            return this.moods[0].tone_id
+        }
+    }
+}
