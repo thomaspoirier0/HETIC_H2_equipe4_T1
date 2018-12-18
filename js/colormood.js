a38577b0689b8975ee7f41e910f8f7da12b353ac
fxmanceau <francois-xavier.manceau@hetic.net>
2018-12-18 21:23:24 +0100

    :sparkles: Add audio Mood & other pages darkmode

--- js/colormood.js
+++ js/colormood.js
@@ -3,27 +3,37 @@ class colorMood
     constructor(mood)
     {
         this.mood = mood
+        this.audioMood = new Audio()
         this.chooseColorPalette()
     }
 
     chooseColorPalette()
     {
+        this.audioMood.loop = true
         switch(this.mood)
         {
             case 'sadness':
                 this.changeWaveesColor(['#D5B8FF', '#BF55EC', '#F1E7FE', '#8E44AD', waves[waves.length-1].colour])
+                this.audioMood.src = '../audio/emotion-sadness.mp3'
+                this.audioMood.play()
                 break;
 
             case 'anger':
                 this.changeWaveesColor(['#F03434', '#EC644B', '#E26A6A', '#F1A9A0', waves[waves.length-1].colour])
+                this.audioMood.src = '../audio/emotion-anger.mp3'
+                this.audioMood.play()
                 break;
             
             case 'fear':
                 this.changeWaveesColor(['#87D37C', '#C8F7C5', '#7BEFB2', '#86E2D5', waves[waves.length-1].colour])
+                this.audioMood.src = '../audio/emotion-fear.mp3'
+                this.audioMood.play()
                 break;
 
             case 'joy':
                 this.changeWaveesColor(['#FFFFCC', '#FFF68F', '#FFF9DE', '#FFFF7E', waves[waves.length-1].colour])
+                this.audioMood.src = '../audio/emotion-joy.mp3'
+                this.audioMood.play()
                 break;
         }
     }
