cd00d268cbe53a0f2f0c4b24ac2083ed7adab75f
fxmanceau <francois-xavier.manceau@hetic.net>
2018-12-18 20:15:42 +0100

    :bug: Last wave color set up

--- js/colormood.js
+++ js/colormood.js
@@ -11,19 +11,19 @@ class colorMood
         switch(this.mood)
         {
             case 'sadness':
-                this.changeWaveesColor(['#D5B8FF', '#BF55EC', '#F1E7FE', '#8E44AD','#FFFFFF'])
+                this.changeWaveesColor(['#D5B8FF', '#BF55EC', '#F1E7FE', '#8E44AD', waves[waves.length-1].colour])
                 break;
 
             case 'anger':
-                this.changeWaveesColor(['#F03434', '#EC644B', '#E26A6A', '#F1A9A0','#FFFFFF'])
+                this.changeWaveesColor(['#F03434', '#EC644B', '#E26A6A', '#F1A9A0', waves[waves.length-1].colour])
                 break;
             
             case 'fear':
-                this.changeWaveesColor(['#87D37C', '#C8F7C5', '#7BEFB2', '#86E2D5', '#FFFFFF'])
+                this.changeWaveesColor(['#87D37C', '#C8F7C5', '#7BEFB2', '#86E2D5', waves[waves.length-1].colour])
                 break;
 
             case 'joy':
-                this.changeWaveesColor(['#FFFFCC', '#FFF68F', '#FFF9DE', '#FFFF7E', '#FFFFFF'])
+                this.changeWaveesColor(['#FFFFCC', '#FFF68F', '#FFF9DE', '#FFFF7E', waves[waves.length-1].colour])
                 break;
         }
     }
