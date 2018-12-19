47e1cd81f23af9801556641569178ae05b52e7af
ThoPoi <thomas.poirier@hetic.net>
2018-12-19 01:16:39 +0100

    clean and commet insertTextArena js

--- js/insertTextArena.js
+++ js/insertTextArena.js
@@ -1,31 +1,27 @@
 const $storyAreaRead = document.querySelector('.area-read p')
 const $labelAuthor = document.querySelector('.label-author p')
 
+/*
+** insert story, name and color mood
+** manage loader
+*/
 const insertTextArena = (message) =>
 {
-    getPost().then(response => {
-
+    getPost().then(response => 
+        {
         if(response.message)
         {
             removeLoaderPost()
-            console.log(response.message)
-            console.log(response.author)
             $storyAreaRead.innerHTML = response.message
             $labelAuthor.innerHTML = response.author
-            const moodInterpreter = new MoodInterpreter(response.mood)
             console.log(moodInterpreter.dominantMood)
             const newColorMood = new colorMood(moodInterpreter.dominantMood)
-
             timeLeft(response.readtime)
         }
         else
         {
             insertTextArena()   
         }
-        
-
-
-
 
     })
 
