7bd07e17dc9d91e5700d8a5d318bf99ec467f234
fxmanceau <francois-xavier.manceau@hetic.net>
2018-12-19 02:00:08 +0100

    :sparkles: Add audio switch

--- js/colormood.js
+++ js/colormood.js
@@ -10,6 +10,7 @@ class colorMood
         this.mood = mood
         this.audioMood = new Audio()
         this.audioMood.volume = 0.5
+        this.audioPlaying = false
         this.chooseColorPalette()
     }
     // choose wawe color and sound
@@ -21,27 +22,40 @@ class colorMood
             case 'sadness':
                 this.changeWaveesColor(['#D5B8FF', '#BF55EC', '#F1E7FE', '#8E44AD', waves[waves.length-1].colour])
                 this.audioMood.src = '../audio/emotion-sadness.mp3'
-                this.audioMood.play()
                 break;
 
             case 'anger':
                 this.changeWaveesColor(['#F03434', '#EC644B', '#E26A6A', '#F1A9A0', waves[waves.length-1].colour])
                 this.audioMood.src = '../audio/emotion-anger.mp3'
-                this.audioMood.play()
                 break;
             
             case 'fear':
                 this.changeWaveesColor(['#87D37C', '#C8F7C5', '#7BEFB2', '#86E2D5', waves[waves.length-1].colour])
                 this.audioMood.src = '../audio/emotion-fear.mp3'
-                this.audioMood.play()
                 break;
 
             case 'joy':
                 this.changeWaveesColor(['#FFFFCC', '#FFF68F', '#FFF9DE', '#FFFF7E', waves[waves.length-1].colour])
                 this.audioMood.src = '../audio/emotion-joy.mp3'
-                this.audioMood.play()
                 break;
         }
+        this.audioMood.play()
+        this.audioButton.addEventListener('click', () =>
+        {
+            console.log('click')
+            console.log(this.audioMood.played)
+            if(this.audioPlaying)
+            {
+                this.audioPlaying = false
+                this.audioMood.pause()
+                this.audioButton.src = '../images/svg/sound.svg'
+            }
+            else{
+                this.audioPlaying = true
+                this.audioMood.play()
+                this.audioButton.src = '../images/svg/no-sound.svg'
+            }
+        })
     }
     // apply wawe color
     changeWaveesColor(colorArray)
